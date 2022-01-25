8006899066a477fd036a5381c8bee635a4f6438c
Akhil <akhilu5042@gmail.com>
2022-01-25 13:32:23 +0530

    UC3_EnterEmail.cs

---
+++ UC3_EnterEmail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReGex_UserRegistration
+{
+    public class UC3_EnterEmail
+    {
+        public string Regex_Pattern = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
+        public bool validateEmail(string Email)
+        {
+            return Regex.IsMatch(Email, Regex_Pattern);
+        }
+    }
+}
