ace9d3192052d6627f2872351b22afd9a0943341
Akhil <akhilu5042@gmail.com>
2022-01-25 13:45:44 +0530

    UC9_CheckDifferentEmail.cs

---
+++ UC9_CheckDifferentEmail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReGex_UserRegistration
+{
+    public class UC9_CheckDifferentEmail
+    {
+        public string Regex_Pattern = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
+        public bool validatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, Regex_Pattern);
+        }
+    }
+}
