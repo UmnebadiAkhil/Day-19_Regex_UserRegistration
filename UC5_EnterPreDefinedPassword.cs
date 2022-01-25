ccff59a64ab564f9b92be3e88328b9124653634e
Akhil <akhilu5042@gmail.com>
2022-01-25 13:35:19 +0530

    UC5_EnterPreDefinedPassword.cs

---
+++ UC5_EnterPreDefinedPassword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReGex_UserRegistration
+{
+    public class UC5_EnterPreDefinedPassword
+    {
+        public string Regex_Pattern = @"[a-z,A-Z,0-9]{8,}$";
+
+        public bool validatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, Regex_Pattern);
+        }
+    }
+}
