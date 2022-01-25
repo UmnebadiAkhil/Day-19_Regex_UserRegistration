4939163c056a7a88efe9fdd6a3dbff0971e96d23
Akhil <akhilu5042@gmail.com>
2022-01-25 13:40:38 +0530

    UC8_Password_Rule-04.cs

---
+++ UC8_Password_Rule-04.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReGex_UserRegistration
+{
+    internal class UC8_Password_Rule_04
+    {
+        public string Regex_Pattern = "^.*(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
+
+        public bool validatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, Regex_Pattern);
+        }
+    }
+}
