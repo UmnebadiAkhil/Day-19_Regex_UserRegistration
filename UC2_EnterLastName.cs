945aff0a8a55f9d6f6b3f71aca84d77888f3f7fd
Akhil <akhilu5042@gmail.com>
2022-01-25 13:30:54 +0530

    UC2_EnterLastName.cs

---
+++ UC2_EnterLastName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReGex_UserRegistration
+{
+    public class UC2_EnterLastName
+    {
+        public string Regex_Pattern = "^[A-Za-z]{3,}$";
+
+        public bool validateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, Regex_Pattern);
+        }
+    }
+}
